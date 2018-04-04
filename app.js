23dc6cdd50f02f152f5d0f55b8de5ce98076e256
Violetta Runaeva <runaevavioletta@gmail.com>
2018-04-04 22:58:26 +0300

    heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express          =   require('express');
 var app              =   express();
 var http             =   require('http');
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 //var port             =   process.env.PORT || 8080;
 var mongoose         =   require('mongoose');
 var mongo            =   require("mongodb");
