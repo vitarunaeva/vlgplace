fe1b223cdcce1540db069ff0ac504615a229d402
Violetta Runaeva <runaevavioletta@gmail.com>
2018-05-28 23:04:05 +0300

    db config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //'url': 'mongodb://admin:admin@ds231549.mlab.com:31549/vlgplace'
-    'url' : 'mongodb://127.0.0.1:27017/vlg_place'
+    'url': 'mongodb://admin:admin@ds231549.mlab.com:31549/vlgplace'
+    //'url' : 'mongodb://127.0.0.1:27017/vlg_place'
 
 };
